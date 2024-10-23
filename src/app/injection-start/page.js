0c949bee9a9e0482fb0b557d4bdccbe009538874
Ilya Kolebayev <kolebayev@gmail.com>
2024-10-23 11:37:45 +0200

    Add validation for injection page

--- src/app/injection-start/page.js
+++ src/app/injection-start/page.js
@@ -1,5 +1,5 @@
 'use client';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Button, Input } from '@lemonsqueezy/wedges';
 import Link from 'next/link';
 
@@ -15,9 +15,15 @@ export default function Home() {
   const nodeEndpoint = NODE_ENDPOINT.use();
   const nodeEndpoint2 = NODE_ENDPOINT_2.use();
 
+  // error state for input
+  const [input1, setInput1] = useState(false);
+  const [input2, setInput2] = useState(false);
+
   useEffect(() => {
     CLEAR_METHODS_DATA();
   }, []);
+
+  let emptyInput = { border: '1px solid red' };
 
   return (
     <div
@@ -32,22 +38,24 @@ export default function Home() {
         <div className="flex gap-4">
           <div className="flex-grow">
             <Input
-              required
+              style={input1 === true ? emptyInput : {}}
               className="fix-input-bg"
               placeholder="Endpoint 1"
               value={nodeEndpoint}
               onChange={(e) => {
+                setInput1(false);
                 SET_NODE_ENDPOINT(e.target.value);
               }}
             />
           </div>
           <div className="flex-grow">
             <Input
-              required
+              style={input2 === true ? emptyInput : {}}
               className="flex-1 fix-input-bg"
               placeholder="Endpoint 2"
               value={nodeEndpoint2}
               onChange={(e) => {
+                setInput2(false);
                 SET_NODE_ENDPOINT_2(e.target.value);
               }}
             />
@@ -55,11 +63,29 @@ export default function Home() {
         </div>
 
         <Link
-          href={{
-            pathname: '/injection-result-double',
-          }}
+          href={
+            input1 === false &&
+            input2 === false &&
+            nodeEndpoint.length !== 0 &&
+            nodeEndpoint2.length !== 0
+              ? {
+                  pathname: '/injection-result-double',
+                }
+              : { pathname: 'javascript:void(0);' }
+          }
         >
-          <Button variant="primary" className="fix-cta-button w-full mt-4">
+          <Button
+            variant="primary"
+            className="fix-cta-button w-full mt-4"
+            onClick={() => {
+              if (nodeEndpoint.length === 0) {
+                setInput1(true);
+              }
+              if (nodeEndpoint2.length === 0) {
+                setInput2(true);
+              }
+            }}
+          >
             Run test →
           </Button>
         </Link>
