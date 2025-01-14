e18ae7ef8860e0171465f77488b8b4bde629db19
smypmsa <antonsauchyk@gmail.com>
2025-01-14 14:17:05 +0000

    feat: add Grafana dashboard button to header

--- next.config.js
+++ next.config.js
@@ -3,11 +3,10 @@ const nextConfig = {
   async headers() {
     return [
       {
-        // matching all API routes
         source: '/api/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' }, // replace this your actual origin
+          { key: 'Access-Control-Allow-Origin', value: '*' },
           {
             key: 'Access-Control-Allow-Methods',
             value: 'GET,DELETE,PATCH,POST,PUT',
@@ -19,6 +18,15 @@ const nextConfig = {
           },
         ],
       },
+    ];
+  },
+  async redirects() {
+    return [
+      {
+        source: '/dashboard',
+        destination: 'https://chainstack.grafana.net/public-dashboards/65c0fcb02f994faf845d4ec095771bd0?orgId=1',
+        permanent: true
+      }
     ];
   },
   reactStrictMode: false,
